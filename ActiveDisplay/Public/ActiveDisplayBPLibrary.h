c969ad835ee02c1de4d1a15a63ef161865b8eba6
Erpan "Wolf" Yusup <erpan9354@hotmail.com>
2023-09-04 04:08:34 +1200

    Update ActiveDisplayBPLibrary.h
    Tidy up some typo for more readability

--- ActiveDisplay/Public/ActiveDisplayBPLibrary.h
+++ ActiveDisplay/Public/ActiveDisplayBPLibrary.h
@@ -3,23 +3,25 @@
 
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "Engine.h"
-#include "SWindow.h" // For Newer Version of Unreal, Use Widgets/SWindow.h
+#include "SWindow.h" // For Newer Version of Unreal, Use Widgets/SWindow.h instead
 #include "ActiveDisplayBPLibrary.generated.h"
 
 /* 
-*	Function library class to change active display for your game.
-*	If Functions are not working correctly or it's outdated. Please email me.
+* Function library class to change active display for your game.
+* If Functions are not working correctly or it's outdated. Please email me.
 *	
-*	Authur: Erpan "Wolf" Yusup
-*   Creation Date: 05 / 06 / 2019
-*   Last Edit Date: 15/ 04 / 2022
-*	Email: Erpan9354@hotmail.com
-*	Website: https://www.erpan.me
+* Authur: Erpan "Wolf" Yusup
+* Creation Date:  05 / 06 / 2019
+* Last Edit Date: 09 / 04 / 2023
+* Email: Erpan9354@hotmail.com
+* Website: https://www.erpan.me
 */
 
 /*
 *  LOG
-*  05/ 06 / 2019 : Created Active Display Library
+*  05 / 06 / 2019 : Created Active Display Library
+*  15 / 04 / 2022 : added comment for newer version of Unreal
+*  09 / 04 / 2023 : tidy up some typo for more readablitiry.
 *  Commented few out lated code for newer version of Unreal
 */
 
@@ -40,7 +42,7 @@ public:
 
 	//Cycle to next active monitor, go back to 0 at the end.
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Cycle To Next Display", Keywords = "Monitor, Display, Screen"), Category = "Active Display")
-		static int32 CycleToNextDisplay(int32 Index);
+	static int32 CycleToNextDisplay(int32 Index);
 
 protected:
 
